6b972f0b34a1fb566b41353af7b323798a5dd441
sebastian <sebastian@trilobyte.de>
2023-11-24 12:16:43 +0100

    Update NullstellenFinderTest for improved accuracy
    Updated expected calculations in NullstellenFinderTest to improve mathematical accuracy. Test functions for finding roots (Nullstelle) have been revised with updated values. A new test function (DoubleFunction i) has also been added.

--- src/test/java/funktionen/NullstellenFinderTest.java
+++ src/test/java/funktionen/NullstellenFinderTest.java
@@ -8,34 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NullstellenFinderTest {
-    @Test
-    void testFindeNullstelle() {
-        assertEquals(5, Math.abs(NullstellenFinder.suchenNullstelle(x -> x * x - 25, -10d, 10d)), 0.01);
-        assertEquals(Math.log(7)/3, NullstellenFinder.suchenNullstelle(x -> Math.pow(Math.E, 3 * x) - 7, 0d, 2d), 0.01);
-        assertEquals(Math.sqrt(Math.asin(0.5)), NullstellenFinder.suchenNullstelle(x -> Math.sin(x * x) - 0.5, 0d, 2d), 0.01);
-        assertThrows(IllegalArgumentException.class, () -> NullstellenFinder.suchenNullstelle(x -> x * x + 1, 0d, 2d));
-    }
-
-    @Test
-    void testNullstelle() {
-        assertEquals(5, Math.abs(NullstellenFinder.suchenNullstelle((x -> x * x - 25), -10d, 10d)),0.01);
-        assertEquals(Math.log(7)/3, NullstellenFinder.suchenNullstelle((x -> Math.exp(3 * x) - 7), 0d, 1d),0.01);
-        assertEquals(-1.7320508075688772, NullstellenFinder.suchenNullstelle((x -> Math.sin(x * x) - 0.5), -2d, 2d));
-
-        // Die Funktion k(x) = x² + 1 hat keine Nullstelle im Intervall [-1, 1].
-        assertEquals(Double.NaN, NullstellenFinder.suchenNullstelle((x -> x * x + 1), -1d, 1d));
-    }
 
     @Test
     void testenNullstelle() {
         DoubleFunction<Double> f = x -> Math.pow(x, 2) - 25;
-        assertEquals(5, Math.abs(NullstellenFinder.suchenNullstelle(f, -10.0,10.0)), 0.01);
+        assertEquals(5, NullstellenFinder.suchenNullstelle(f, 0.0,10.0), 0.01);
 
         DoubleFunction<Double> g = x -> Math.exp(3*x) - 7;
-        assertEquals(0.694, NullstellenFinder.suchenNullstelle(g, 0.0,5.0), 0.01);
+        assertEquals(0.648, NullstellenFinder.suchenNullstelle(g, 0.0,5.0), 0.01);
 
         DoubleFunction<Double> h = x -> Math.sin(Math.pow(x, 2)) - 0.5;
-        assertEquals(0.824, NullstellenFinder.suchenNullstelle(h, 0.0,2.0), 0.01);
+        assertEquals(0.723, NullstellenFinder.suchenNullstelle(h, 0.0,2.0), 0.01);
+
+        DoubleFunction<Double> i = x -> -(Math.pow(x,2)) + 31;
+        assertEquals(5.567, NullstellenFinder.suchenNullstelle(i,0d,7d),0.01);
 
         DoubleFunction<Double> k = x -> Math.pow(x, 2) + 1;
         try {
